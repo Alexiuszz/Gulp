55d06bf23647b6238313020802ad47dd8869b74a
Nwala Enyinna <65119080+Alexiuszz@users.noreply.github.com>
2023-10-11 10:02:03 +0100

    Complete Tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const { series, dest } = require("gulp");
 const { EventEmitter } = require("events");
+const { Observable } = require("rxjs");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const gulpif = require('gulp-if');
 
 function transpile(cb) {
   cb;
@@ -30,4 +34,51 @@ function eventTask() {
   }, 250);
   return emitter;
 }
-exports.build = series(t);
+
+//return a child process
+const { exec } = require("child_process");
+
+function childProcessTask() {
+  return exec("date");
+}
+
+// Returning an observable
+
+function observableTask() {
+  return Observable.of(1, 2, 3);
+}
+
+// async await task
+async function asyncAwaitTask() {
+  const { version } = JSON.parse(
+    fs.readFileSync("package.json", "utf8")
+  );
+  console.log(version);
+  await Promise.resolve("some result");
+}
+
+function MultipleEntry() {
+  return src("src/*.js")
+    .pipe(babel())
+    .pipe(src("vendor/*.js"))
+    .pipe(uglify())
+    .pipe(dest("output/"));
+};
+
+const { src, dest } = require('gulp');
+const gulpif = require('gulp-if');
+const uglify = require('gulp-uglify');
+
+function isJavaScript(file) {
+  // Check if file extension is '.js'
+  return file.extname === '.js';
+}
+
+// Gulp if plugin
+function GulpifPlugin() {
+  // Include JavaScript and CSS files in a single pipeline
+  return src(['src/*.js', 'src/*.css'])
+    // Only apply gulp-uglify plugin to JavaScript files
+    .pipe(gulpif(isJavaScript, uglify()))
+    .pipe(dest('output/'));
+}
